b05f47a0b1dd9e42d2a519f4fe85692ba5074949
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-07-25 12:51:01 +0100

    update page

---
+++ js/script.js
@@ -0,0 +1,18 @@
+openMenu = document.querySelector(".nav__open");
+closeMenu = document.querySelector(".nav__close");
+navMenu = document.querySelector(".nav__menu");
+navItems = document.querySelectorAll(".nav__item");
+
+openMenu.addEventListener("click", ()=>{
+    navMenu.classList.toggle("toggle");
+});
+
+closeMenu.addEventListener("click", ()=> {
+    navMenu.classList.remove("toggle");
+});
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navMenu.classList.remove("toggle");
+    })
+});
